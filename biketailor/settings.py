4d07de9c2788764b5db82011ee37644675590c47
Calum J. Eadie <calum@calumjeadie.com>
2013-10-07 21:37:15 +0100

    Added bootstrap 3

--- biketailor/settings.py
+++ biketailor/settings.py
@@ -127,7 +127,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'biketailor'
+    'biketailor',
+    'bootstrap3'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
