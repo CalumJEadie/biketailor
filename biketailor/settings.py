cf513d24247621cec5fedcc790eea94b1f4847fa
Calum J. Eadie <calum@calumjeadie.com>
2013-10-07 22:15:47 +0100

    Removed mistakenly included bootstrap 3 template

--- biketailor/settings.py
+++ biketailor/settings.py
@@ -127,8 +127,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'biketailor',
-    'bootstrap3'
+    'biketailor'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
