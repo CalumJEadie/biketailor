cd8c4e5cccf4e18ac4eee568367b6940d8d1b274
Calum J. Eadie <calum@calumjeadie.com>
2013-10-08 12:11:50 +0100

    Added jumbotron template

--- biketailor/urls.py
+++ biketailor/urls.py
@@ -16,5 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', TemplateView.as_view(template_name="home.html"))
+    url(r'^$', TemplateView.as_view(template_name="home.html")),
+
+    url(r'^home2/$', TemplateView.as_view(template_name="home2.html"))
 )
